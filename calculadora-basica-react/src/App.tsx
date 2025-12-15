60a4e08198081e8ff60ed950d2e143501d07c8fe
Taki <antonia.luengo.silva@gmail.com>
2025-12-15 15:02:15 -0300

    traspasar css y html a react (css y tsx)

--- calculadora-basica-react/src/App.tsx
+++ calculadora-basica-react/src/App.tsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  return ( 
+    /* A diferencia del codigo html, se quitan todas las etiquetas que indiquen cosas extra que no sean solo de la calculadora*/ 
+    /* todo lo demás esta definido automaticamente por react */
+    <div className=" calculadora"> 
+        <h1>Calculadora Básica</h1>
+        <div className="pantalla">
+            <div className="operacion"></div> {/*muestra la operación (entre llaves para comentarios de jsx)*/}
+            <div className="resultado">0</div> {/*muestra el resultado*/}
+        </div>
+        <div className = "botones">
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>Del</button>
+            <button>AC</button>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>*</button>
+            <button>/</button>
+
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>+</button>
+            <button>-</button>
+            <button className="zero">0</button> {/*Dar al cero una clase unica para poder cambiar su ancho */}
+            <button>.</button>
+            <button>=</button>
+        </div>
+    </div>
   )
 }
 
