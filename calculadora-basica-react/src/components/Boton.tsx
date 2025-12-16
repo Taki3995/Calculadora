109fee558c11769bac32c35750adfe0047f5c3b7
Taki <antonia.luengo.silva@gmail.com>
2025-12-16 20:47:36 -0300

    correccion botones de otros colores

--- calculadora-basica-react/src/components/Boton.tsx
+++ calculadora-basica-react/src/components/Boton.tsx
@@ -5,9 +5,11 @@ interface BotonProps {
 }
 
 function Boton({ texto, clase, manejarClic }: BotonProps) {
+    const colorBase = clase?.includes('bg-') ? '' : 'bg-indigo-100';
+    const colorHover = clase?.includes('hover:bg-') ? '' : 'hover:bg-indigo-300'; // si no le doy yo una clase al boton, toma esta default
     return (
         <button
-            className={`size-14 w-full aspect-square bg-indigo-200 text-2xl rounded-xl shadow hover:bg-indigo-500 ${clase}`}
+            className={`size-14 w-full aspect-square ${colorBase} text-2xl rounded-xl shadow ${colorHover} ${clase}`}
             onClick={() => manejarClic(texto)}>
             {texto}
         </button>
